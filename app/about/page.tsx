9a95da294b5eab2863b83b60baf879a20259ac8c
한상일 <hansang-il@MacBookAir.localdomain>
2025-04-14 12:27:42 +0900

    [Modify] Header, Footer Style Modify

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,3 +1,4 @@
+// 프로젝트 공지 관련 이미지 등 을 배치하는 목적
 export default function page() {
   return (
     <>
--- components/Common/Header.tsx
+++ components/Common/Header.tsx
@@ -1,8 +1,14 @@
+import Link from "next/link";
+
 export default function Header() {
   return (
     <>
-      <header className="w-full border-b-2 border-slate-300 h-10 my-2">
-        <h1 className="font-bold text-violet-500">Header</h1>
+      <header className="w-full border-b-2 border-slate-300 h-10 my-2 flex flex-row justify-between">
+        <Link href='/' className="font-bold text-xl text-violet-500">ProjectLogo</Link>
+
+        <section>
+          <Link href='/about' className="font-bold text-violet-500 text-sm px-4 cursor-pointer">공지사항</Link>
+        </section>
       </header>
     </>
   );
