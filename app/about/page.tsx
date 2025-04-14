5b6bcc5d511b4135310b3b2d5a1c5bbdf9ebfd6d
한상일 <hansang-il@MacBookAir.localdomain>
2025-04-14 12:48:07 +0900

    [Modify] app/about/page.tsx 초안 수정

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,8 +1,19 @@
+import Link from 'next/link';
 // 프로젝트 공지 관련 이미지 등 을 배치하는 목적
+
 export default function page() {
   return (
-    <>
-      <h1 className="text-red-600 font-bold">1</h1>
-    </>
+    <div className="w-full flex flex-col justify-center items-center">
+      <h1 className="text-red-600 text-xl font-bold">사이트 이용 참고</h1>
+
+      <p className="text-white font-bold py-4">다음의 사례는 등장하지 않습니다</p>
+
+      <p className="text-white font-bold py-4">1. PUBG처럼 무료인 경우</p>
+      <p className="text-white font-bold py-4">2. 태그에 선정적 이 존재할 경우</p>
+      <Link href="https://store.steampowered.com/charts/topselling/KR" target="_blank" className="text-white font-bold">
+        {' '}
+        기준은 여기를 참고합니다 [링크 : 바로 가기]
+      </Link>
+    </div>
   );
 }
--- components/Common/Header.tsx
+++ components/Common/Header.tsx
@@ -1,13 +1,17 @@
-import Link from "next/link";
+import Link from 'next/link';
 
 export default function Header() {
   return (
     <>
       <header className="w-full border-b-2 border-slate-300 h-10 my-2 flex flex-row justify-between">
-        <Link href='/' className="font-bold text-xl text-violet-500">ProjectLogo</Link>
+        <Link href="/" className="font-bold text-xl text-violet-500">
+          ProjectLogo
+        </Link>
 
         <section>
-          <Link href='/about' className="font-bold text-violet-500 text-sm px-4 cursor-pointer">공지사항</Link>
+          <Link href="/about" className="font-bold text-violet-500 text-sm px-4 cursor-pointer">
+            공지사항
+          </Link>
         </section>
       </header>
     </>
