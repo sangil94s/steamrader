4ec9501ddab440676f92036716c282076694f693
sangil94s <bsc7417@gmail.com>
2025-04-19 08:03:27 +0900

    [Add] 임박한 세일 일정 알려주는 컴포넌트 추가

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 import CardList from '@/components/Card/CardList';
+import Sales from '@/components/News/Sales';
 
 export default function Home() {
   return (
     <div>
+      <Sales />
       <CardList />
     </div>
   );
---
+++ components/News/Sales.tsx
@@ -0,0 +1,14 @@
+// 스팀 할인 임박한거 한줄로 보여줍니다
+// https://store.steampowered.com/news/group/4145017/view/4264427597939246983?l=koreana
+// https://store.steampowered.com/news/group/4145017/view/497187349280587903?l=koreana
+export default function Sales() {
+  return (
+    <>
+      <div>
+        <p className="text-white text-center font-bold py-4">
+          🛒 임박한 할인 일정 - 창고지기 게임 축제 : 4월 21일 - 4월 28일
+        </p>
+      </div>
+    </>
+  );
+}
