4713da7834153886a8d517723325ba616ce109fc
sangil94s <bsc7417@gmail.com>
2025-04-22 19:45:52 +0900

    [Add] 필터링 선택 목적의 Select 초안 추가

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,21 @@
 import CardList from '@/components/Card/CardList';
 import Sales from '@/components/News/Sales';
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
 
 export default function Home() {
   return (
     <div>
       <Sales />
+      <Select>
+        <SelectTrigger className="w-[180px] m-2">
+          <SelectValue placeholder="Theme" />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectItem value="light">Light</SelectItem>
+          <SelectItem value="dark">Dark</SelectItem>
+          <SelectItem value="system">System</SelectItem>
+        </SelectContent>
+      </Select>
       <CardList />
     </div>
   );
