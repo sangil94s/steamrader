a46f213f367d801969fd0746988245b3b3871ba7
sangil94s <bsc7417@gmail.com>
2025-04-25 20:18:32 +0900

    [Remove] 미 사용 파일 제거

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,10 @@
 import CardList from '@/components/Card/CardList';
-import GenreSelect from '@/components/Genre/GenreSelect';
 import Sales from '@/components/News/Sales';
 
 export default function Home() {
   return (
     <div>
       <Sales />
-      <GenreSelect />
       <CardList />
     </div>
   );
