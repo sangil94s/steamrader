a74aa04b375b486ff872c822fe6c73b58fec41c6
한상일 <hansang-il@MacBookAir.localdomain>
2025-05-16 09:51:18 +0900

    [Add] Loading.tsx

---
+++ app/loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <div className="w-12 h-12 border-4 border-gray-300 border-t-blue-500 rounded-full animate-spin" />
+    </div>
+  );
+}
